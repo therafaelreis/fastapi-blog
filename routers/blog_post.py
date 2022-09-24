d67f0b591e51337c1ece2d0cecb431ef1cbca9d8
4KXP <therafaelreis@gmail.com>
2022-09-24 14:09:10 -0500

    :::added path params for the post create comment

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,10 +1,43 @@
-from fastapi import APIRouter
+from typing import Optional, List
+from fastapi import APIRouter, Query, Body, Path
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/blog',
     tags=['blog']
 )
 
-@router.post('/new')
-def create_blog():
-    pass
+
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+    number_of_comments: int
+
+
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        'id': id,
+        'data': blog,
+        'version': version
+    }
+
+
+@router.post('/new/{id}/comment/{comment_id}')
+def create_comment(blog: BlogModel, id: int, comment_title: int = Query(None,
+                                                                     title='Title of the comment',
+                                                                     description='Some description for comment title',
+                                                                     alias='commentTitle',
+                                                                     deprecated=True),
+                                                                     content: str = Body(...),
+                                                                     v: Optional[List[str]] = Query(None),
+                                                                     comment_id: int = Path(None, gt=5, le=10)):
+    return {
+        'id': id,
+        'data': blog,
+        'comment_title': comment_title,
+        'content': content,
+        'comment_id': comment_id,
+        'version': v
+    }
