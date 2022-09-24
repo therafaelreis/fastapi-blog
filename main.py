a7457d079e595c4c20908cad92b97e77b72d034a
4KXP <therafaelreis@gmail.com>
2022-09-24 16:16:15 -0500

    :::added retrieve operation

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 from routers import blog_get
 from routers import blog_post
+from routers import user
+from db import models
+from db.database import engine
+
 app = FastAPI()
 
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+app.include_router(user.router)
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
 
 
-
+models.Base.metadata.create_all(engine)
