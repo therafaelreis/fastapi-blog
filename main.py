1980e1119fbd287533feae42fd530e96d54a42a6
4KXP <therafaelreis@gmail.com>
2022-09-23 21:22:52 -0500

    :::start

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from routers import blog_get
+from routers import blog_post
+app = FastAPI()
+
+app.include_router(blog_get.router)
+app.include_router(blog_post.router)
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+
+
