4c86a959b175888f7c5a69ce95e5493ec12c7153
4KXP <therafaelreis@gmail.com>
2022-09-24 17:08:10 -0500

    :::format the document

--- db/models.py
+++ db/models.py
@@ -2,9 +2,10 @@ from db.database import Base
 from sqlalchemy import Column, String
 from sqlalchemy.sql.sqltypes import Integer
 
+
 class DbUser(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key = True, index = True)
+    id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
     email = Column(String)
     password = Column(String)
--- routers/user.py
+++ routers/user.py
@@ -10,29 +10,27 @@ router = APIRouter(
     tags=['user']
 )
 
-# create user
+
 @router.post('/', response_model=UserResponse)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(request, db)
 
-# read user
+
 @router.get('/', response_model=List[UserResponse])
 def get_all_users(db: Session = Depends(get_db)):
     return db_user.get_all_users(db)
 
 
-# read user by id
 @router.get('/{id}', response_model=UserResponse)
 def get_user_by_id(id: int, db: Session = Depends(get_db)):
     return db_user.find_user_by_id(id, db)
 
-# update user
+
 @router.patch('/{id}')
 def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
     return db_user.update_user(id, request, db)
 
 
-# delete user
 @router.delete('/{id}')
 def delete_user(id: int, db: Session = Depends(get_db)):
     return db_user.delete_user(id, db)
