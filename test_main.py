89046e89bc513c0c4bc82f8c52fa6e1e42ef587f
4KXP <therafaelreis@gmail.com>
2022-09-28 17:43:38 -0500

    :::added some unit tests

---
+++ test_main.py
@@ -0,0 +1,50 @@
+from urllib import response
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_get_all_blogs():
+    response = client.get("/blog/all")
+    assert response.status_code == 200
+
+
+def test_auth_error():
+    response = client.post("/token",
+                           data={'username': "", 'password': ""}
+                           )
+    access_token = response.json().get("access_token")
+    assert access_token == None
+    message = response.json().get("detail")[0].get("msg")
+    assert message == "field required"
+
+
+def test_auth_succes():
+    response = client.post("/token",
+                           data={'username': "rafael", 'password': "test"}
+                           )
+    access_token = response.json().get("access_token")
+    assert access_token
+
+
+def test_post_article():
+    auth = client.post("/token",
+                       data={'username': "rafael", 'password': "test"}
+                       )
+    access_token = auth.json().get("access_token")
+
+    response = client.post('/article/',
+                           json={
+                               'title': 'title 1',
+                               'content': 'content 1',
+                               'published': True,
+                               "creator_id": 1,
+                           },
+                           headers={
+                               'Authorization': "Bearer " + access_token
+                           }
+                           )
+
+    assert response.status_code == 200
+    assert response.json().get('title') == "title 1"
